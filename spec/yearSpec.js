e861e64e95f69fc12a30989c1c5c31621b8668ae
Priyadarshini Ganesan <priya@Priyadarshinis-MacBook-Pro.local>
2019-05-07 15:52:01 +0200

    feat: Display error message for special characters

--- spec/yearSpec.js
+++ spec/yearSpec.js
@@ -1,9 +1,17 @@
 const Year = require('../src/year');
 
-describe('validate year', function(){
-    it('should display " Not a valid year. Gregorian Calendar starts from 1582" when we enter 0',function(){
-       
+describe('validate year', function () {
+    it('should display " Not a valid year. Gregorian Calendar starts from 1582" when we enter 0', function () {
+
         var year = new Year(0);
+
+        expect(year.validate()).toBe(false);
+    });
+
+
+    it('should display " Not a valid year. Gregorian Calendar starts from 1582" when we enter special characters', function () {
+
+        var year = new Year(',,,');
 
         expect(year.validate()).toBe(false);
     });
